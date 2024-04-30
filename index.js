6ec4b2ff7edba3eb771763a3e5bc66e9f97a405d
Mari525 <56230146+Mari525@users.noreply.github.com>
2024-04-30 21:46:16 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const items = document.querySelectorAll('.faq__item')
 
 items.forEach((item) => {
-  const btn = item.querySelector(".faq__btn")
-  const answer = item.querySelector(".faq__answer")
+  const btn = item.querySelector('.faq__btn')
+  const answer = item.querySelector('.faq__answer')
+  const question = item.querySelector('.faq__question')
+  const btnOpen = item.querySelector('.faq__svg-plus')
+  const btnClose = item.querySelector('.faq__svg-minus')
 
   btn.addEventListener('click', () => {
     answer.classList.toggle('faq__answer--active')
+    question.classList.toggle('faq__question--active')
+    btnOpen.classList.toggle('faq__btn--hide')
+    btnClose.classList.toggle('faq__btn--hide')
   })
 })
